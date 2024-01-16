7dbd3d78ff9bf72857190a56bc0de6bd2df8ca26
benoitbMTL <benoit@buonassera.fr>
2024-01-16 18:31:47 -0500

    upload page

--- server.js
+++ server.js
@@ -27,10 +27,28 @@ app.get('/upload', (req, res) => {
             res.status(500).send('Unable to scan directory: ' + err);
         } 
         else {
-            res.send(files.join("<br>"));
+            let fileListHtml = files.map(file => `<li><a href="/upload/${file}">${file}</a></li>`).join("");
+            res.send(`
+                <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Uploaded Files</title>
+                    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+                </head>
+                <body>
+                    <div class="container mt-5">
+                        <h2>Uploaded Files</h2>
+                        <ul>${fileListHtml}</ul>
+                    </div>
+                </body>
+                </html>
+            `);
         }
     });
 });
+
 
 app.listen(9000, () => {
     console.log('Server is running on port 9000');
