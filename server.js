646efc5f7da4aa8318bbf1eb5e5d69e4af6d650c
benoitbMTL <benoit@buonassera.fr>
2024-01-16 16:45:55 -0500

    new

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const app = express();
+
+app.use(fileUpload());
+app.use(express.static('public'));
+app.use('/upload', express.static('upload'));
+
+app.post('/upload', (req, res) => {
+    if (!req.files) {
+        return res.status(400).send('No files were uploaded.');
+    }
+
+    let uploadedFile = req.files.file;
+    uploadedFile.mv(`upload/${uploadedFile.name}`, err => {
+        if (err) return res.status(500).send(err);
+        res.send('File uploaded!');
+    });
+});
+
+app.listen(9000, () => {
+    console.log('Server is running on port 9000');
+});
