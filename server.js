ca0abd1c1941ba39c1d4098d6921fce59c7b94f8
benoitbMTL <benoit@buonassera.fr>
2024-01-17 23:43:18 -0500

    new list

--- server.js
+++ server.js
@@ -45,7 +45,11 @@ app.get("/upload", async (req, res) => {
                 <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
             </head>
             <body>
-                    <ul>${fileInfos.join("")}</ul>
+                <ul class="list-group">
+                    ${fileInfos
+                      .map((fileInfo) => `<li class="list-group-item">${fileInfo}</li>`)
+                      .join("")}
+                </ul>
             </body>
             </html>
         `);
@@ -53,9 +57,6 @@ app.get("/upload", async (req, res) => {
     res.status(500).send("Unable to scan directory: " + err);
   }
 });
-
-
-
 
 app.post("/delete-all", async (req, res) => {
   const directoryPath = path.join(__dirname, "upload");
@@ -76,10 +77,6 @@ app.post("/delete-all", async (req, res) => {
       .json({ success: false, message: "Error deleting files: " + err });
   }
 });
-
-
-
-
 
 app.listen(9000, () => {
   console.log("Server is running on port 9000");
